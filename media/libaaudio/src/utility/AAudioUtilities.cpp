86d7383d1b4560f61532f78733feaa9cef187f4e
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-03-24 16:36:07 -0300

    [1/2] av: Remove restrictions for system audio record
    * Give freedom to screen recorder apps
    
    Change-Id: I726bde4f44bba6fc8cd771ae90c8864b26cdd919

--- media/libaaudio/src/utility/AAudioUtilities.cpp
+++ media/libaaudio/src/utility/AAudioUtilities.cpp
@@ -239,6 +239,7 @@ audio_flags_mask_t AAudioConvert_allowCapturePolicyToAudioFlagsMask(
         aaudio_spatialization_behavior_t spatializationBehavior,
         bool isContentSpatialized) {
     audio_flags_mask_t flagsMask = AUDIO_FLAG_NONE;
+    /**
     switch (policy) {
         case AAUDIO_UNSPECIFIED:
         case AAUDIO_ALLOW_CAPTURE_BY_ALL:
@@ -254,7 +255,7 @@ audio_flags_mask_t AAudioConvert_allowCapturePolicyToAudioFlagsMask(
         default:
             ALOGE("%s() 0x%08X unrecognized capture policy", __func__, policy);
             // flagsMask is not modified
-    }
+    }**/
 
     switch (spatializationBehavior) {
         case AAUDIO_UNSPECIFIED:
